5f67861131e6a0a80408ecf6c36e247d376df5f3
Razumovskyi Yurii <GarazdCreation@gmail.com>
2019-04-06 18:44:10 +0300

    [IMP] l10n_ua_num2text: extend description

--- l10n_ua_num2text/__manifest__.py
+++ l10n_ua_num2text/__manifest__.py
@@ -10,7 +10,38 @@
     'license': 'LGPL-3',
     'summary': 'Convert numbers to Ukrainian text',
     'description': """
-        This module allows converting numbers into Ukrainian text (Сума прописом/Сумма прописью).
+This module allows converting numbers into Ukrainian text (Сума прописом/Сумма прописью).
+
+USING:
+======
+
+    import decimal
+
+    from odoo.addons.l10n_ua_num2text import num2t4ua
+
+    ...
+
+    # return string
+
+    num2t4ua.num2text(int(value)))
+
+    ...
+
+    # return string with the currency (грн, коп)
+
+    num2t4ua.decimal2text(decimal.Decimal(value)))
+
+
+Технічна підтримка та розробка
+==============================
+
+Контакти для зв'язку з нами:
+
+* support@garazd.biz
+* `https://garazd.biz/page/contactus`_
+.. _https://garazd.biz/page/contactus: https://garazd.biz/page/contactus
+
+Пакети технічної підтримки: https://garazd.biz/page/odoo-support
     """,
     'images': ['static/description/banner.png'],
     'depends' : [],
